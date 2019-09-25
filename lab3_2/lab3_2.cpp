39fa5914ca379208c4c6541dd7d2ed071e22205f
catprogrammer-cat <catprogrammer.vlad@gmail.com>
2019-09-25 21:41:48 +0300

    add some change

--- lab3_2/lab3_2.cpp
+++ lab3_2/lab3_2.cpp
@@ -9,7 +9,7 @@ int main()
 
 		cout << "Choose the graph:" << endl;
 		cout << "1. tan(sqrt(x))\n";
-		cout << "2. x / pow( (x-3), 2 )\n";
+		cout << "2. (x) / pow((x - 3), 2)\n";
 
 		cin >> choose_graph;
 		cout << "\n";
@@ -19,9 +19,26 @@ int main()
 			for (double x = 4.5; x <= 18.5; x=x+0.4)
 			{
 				result_1 = tan(sqrt(x));
-				cout << result_1 << endl;
+				cout << "x = " << x <<  ";" << " The result = "<< result_1 << ";" << endl;
 			}
-		
+			break;
+			system("pause");
+		}
+		else if (choose_graph == 2)
+		{
+			float result_1;
+			for (double x = 4.5; x <= 18.5; x = x + 0.4)
+			{
+				result_1 = (x) / pow((x - 3), 2);
+				cout << "x = " << x << ";" << " The result = " << result_1 << ";" << endl;
+			}
+			break;
+			system("pause");
+
+		}
+		else {
+			cout << "incorrent input" << endl;
+			
 		}
 	}
 	
